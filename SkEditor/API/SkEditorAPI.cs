7ff81922008a5146a2e59f5c1964394262851228
Sky <livegogo.16334@gmail.com>
2024-05-26 18:33:25 +0200

    :zap: Made SkEditorAPI fields read-only

--- SkEditor/API/SkEditorAPI.cs
+++ SkEditor/API/SkEditorAPI.cs
@@ -9,21 +9,21 @@ public static class SkEditorAPI
     /// <summary>
     /// Get the Logs API.
     /// </summary>
-    public static ILogs Logs = new Logs();
+    public static readonly ILogs Logs = new Logs();
     
     /// <summary>
     /// Get the Windows API.
     /// </summary>
-    public static IWindows Windows = new Windows();
+    public static readonly IWindows Windows = new Windows();
     
     /// <summary>
     /// Get the Core API.
     /// </summary>
-    public static ICore Core = new Core();
+    public static readonly ICore Core = new Core();
     
     /// <summary>
     /// Get the Events API.
     /// </summary>
-    public static IEvents Events = new Events();
+    public static readonly IEvents Events = new Events();
     
 }
