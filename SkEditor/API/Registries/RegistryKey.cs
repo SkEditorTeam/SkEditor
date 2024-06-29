3944d533a4430ba7088821761cb10af9b1d199e1
Sky <livegogo.16334@gmail.com>
2024-06-29 16:06:07 +0200

    :sparkles: Added FullKey for a registry key

--- SkEditor/API/Registries/RegistryKey.cs
+++ SkEditor/API/Registries/RegistryKey.cs
@@ -5,4 +5,12 @@
 /// </summary>
 /// <param name="Addon">The IAddon instance linked to the key.</param>
 /// <param name="Key">The string key.</param>
-public record RegistryKey(IAddon Addon, string Key);
+public record RegistryKey(IAddon Addon, string Key)
+{
+    
+    /// <summary>
+    /// Get the full key, which is the addon identifier followed by a slash and the key.
+    /// </summary>
+    public string FullKey => $"{Addon.Identifier}/{Key}";
+    
+};
