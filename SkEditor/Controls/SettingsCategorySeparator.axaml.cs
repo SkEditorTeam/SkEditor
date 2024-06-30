d0b7013d2f8281edad71204b2d7e9384332912ea
Sky <livegogo.16334@gmail.com>
2024-06-30 12:21:41 +0200

    :zap: Separated items of general page into two categories

---
+++ SkEditor/Controls/SettingsCategorySeparator.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+
+namespace SkEditor.Controls;
+
+public partial class SettingsCategorySeparator : UserControl
+{
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<SettingsCategorySeparator, string>(nameof(Title));
+
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    
+    public SettingsCategorySeparator()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        TitleBlock.Text = Title;
+    }
+}
