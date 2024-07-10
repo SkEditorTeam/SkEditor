33b56af5c227e366d46d107149fc4bbcd5712966
NotroDev <notro-kontakt@wp.pl>
2024-07-10 09:03:54 +0200

    Delete hidden items from marketplace

--- SkEditor/Views/Marketplace/MarketplaceLoader.cs
+++ SkEditor/Views/Marketplace/MarketplaceLoader.cs
@@ -12,7 +12,7 @@ namespace SkEditor.Views.Marketplace;
 public class MarketplaceLoader
 {
     private static readonly string[] supportedTypes = ["NewSyntax", "Theme", "Addon", "NewThemeWithSyntax", "ZipAddon"];
-    private static readonly string[] hiddenItems = ["Shadow", "Analyzer"];
+    private static readonly string[] hiddenItems = [];
 
     public static async IAsyncEnumerable<MarketplaceItem> GetItems()
     {
