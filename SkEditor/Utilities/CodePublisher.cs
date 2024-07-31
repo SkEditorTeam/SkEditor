97c8c2cf975a03bc2f47287368fae6568eaeb7d2
FixeQ <134298056+FixeQyt@users.noreply.github.com>
2024-07-31 22:32:28 +0200

    Add Directory Support and Refactor CodePublisher Methods (Update CodePublisher.cs)

--- SkEditor/Utilities/CodePublisher.cs
+++ SkEditor/Utilities/CodePublisher.cs
@@ -1,4 +1,4 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
 using Newtonsoft.Json.Linq;
 using SkEditor.API;
 using SkEditor.Views;
@@ -6,88 +6,94 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace SkEditor.Utilities;
-public class CodePublisher
+namespace SkEditor.Utilities
 {
-    public static async void PublishPastebin(string code, PublishWindow window)
+    public class CodePublisher
     {
-        try
+        public static async Task PublishPastebin(string code, PublishWindow window)
         {
-            HttpClient client = new();
-            var request = new HttpRequestMessage(HttpMethod.Post, "https://pastebin.com/api/api_post.php");
-            var collection = new List<KeyValuePair<string, string>>
+            try
             {
-                new("api_dev_key", window.ApiKeyTextBox.Text),
-                new("api_paste_code", code),
-                new("api_option", "paste")
-            };
-            var content = new FormUrlEncodedContent(collection);
-            request.Content = content;
-            var response = await client.SendAsync(request);
-            string responseString = await response.Content.ReadAsStringAsync();
+                using HttpClient client = new();
+                var request = new HttpRequestMessage(HttpMethod.Post, "https://pastebin.com/api/api_post.php");
+                var collection = new List<KeyValuePair<string, string>>
+                {
+                    new("api_dev_key", window.ApiKeyTextBox.Text),
+                    new("api_paste_code", code),
+                    new("api_option", "paste")
+                };
+                var content = new FormUrlEncodedContent(collection);
+                request.Content = content;
+                var response = await client.SendAsync(request);
+                string responseString = await response.Content.ReadAsStringAsync();
 
-            string url = responseString;
-            window.ResultTextBox.Text = url;
-        }
-        catch (Exception e)
-        {
-            await SkEditorAPI.Windows.ShowError("Something went wrong.\nDo you have an internet connection? Did you enter correct API key?\n\n" + e.Message);
-        }
-    }
-
-    public static async void PublishCodeSkriptPl(string code, PublishWindow window)
-    {
-        string language = (window.LanguageComboBox.SelectedItem as ComboBoxItem).Content.ToString().ToLower();
-
-        try
-        {
-            string json = JsonSerializer.Serialize(new
+                string url = responseString;
+                window.ResultTextBox.Text = url;
+            }
+            catch (Exception e)
             {
-                key = window.ApiKeyTextBox.Text,
-                language,
-                content = code,
-                anonymous = window.AnonymouslyCheckBox.IsChecked
-            });
-
-            HttpClient client = new();
-            HttpResponseMessage response = await client.PostAsync("https://code.skript.pl/api/v1/codes/create",
-                                                    new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
-            string responseString = await response.Content.ReadAsStringAsync();
-
-            JObject jsonResult = JObject.Parse(responseString);
-            string url = jsonResult["url"].ToString();
-            window.ResultTextBox.Text = url;
+                await SkEditorAPI.Windows.ShowError("Something went wrong.\nDo you have an internet connection? Did you enter correct API key?\n\n" + e.Message);
+            }
         }
-        catch (Exception e)
+
+        public static async Task PublishCodeSkriptPl(string code, PublishWindow window)
         {
-            await SkEditorAPI.Windows.ShowError("Something went wrong.\nDo you have an internet connection? Did you enter correct API key?\n\n" + e.Message);
+            string language = (window.LanguageComboBox.SelectedItem as ComboBoxItem).Content.ToString().ToLower();
+            bool? anonymousChecked = window.AnonymouslyCheckBox.IsChecked;
+            string directory = !anonymousChecked.HasValue || !anonymousChecked.Value ? "SkEditor_content" : null;
+
+            try
+            {
+                string json = JsonSerializer.Serialize(new
+                {
+                    key = window.ApiKeyTextBox.Text,
+                    language,
+                    content = code,
+                    anonymous = anonymousChecked,
+                    directory
+                });
+
+                using HttpClient client = new();
+                HttpResponseMessage response = await client.PostAsync("https://code.skript.pl/api/v1/codes/create",
+                                                new StringContent(json, System.Text.Encoding.UTF8, "application/json"));
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                JObject jsonResult = JObject.Parse(responseString);
+                string url = jsonResult["url"].ToString();
+                window.ResultTextBox.Text = url;
+            }
+            catch (Exception e)
+            {
+                await SkEditorAPI.Windows.ShowError("Something went wrong.\nDo you have an internet connection? Did you enter correct API key?\n\n" + e.Message);
+            }
         }
-    }
 
-    public static async void PublishSkUnity(string code, PublishWindow window)
-    {
-        try
+        public static async Task PublishSkUnity(string code, PublishWindow window)
         {
-            string apiKey = window.ApiKeyTextBox.Text;
-            string json = JsonSerializer.Serialize(new { content = code });
-            string fileName = SkEditorAPI.Files.GetCurrentOpenedFile().Name;
+            try
+            {
+                string apiKey = window.ApiKeyTextBox.Text;
+                string json = JsonSerializer.Serialize(new { content = code });
+                string fileName = SkEditorAPI.Files.GetCurrentOpenedFile().Name;
 
-            HttpClient client = new();
-            HttpRequestMessage request = new(HttpMethod.Post, $"https://api.skunity.com/v1/{apiKey}/parser/savenewfile/{fileName}");
-            request.Headers.Add("User-Agent", "SkEditor");
-            request.Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                using HttpClient client = new();
+                HttpRequestMessage request = new(HttpMethod.Post, $"https://api.skunity.com/v1/{apiKey}/parser/savenewfile/{fileName}");
+                request.Headers.Add("User-Agent", "SkEditor");
+                request.Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await client.SendAsync(request);
-            string responseString = await response.Content.ReadAsStringAsync();
+                HttpResponseMessage response = await client.SendAsync(request);
+                string responseString = await response.Content.ReadAsStringAsync();
 
-            JObject jsonResult = JObject.Parse(responseString);
-            string url = "https://parser.skunity.com/" + jsonResult["result"]["share_code"].ToString();
-            window.ResultTextBox.Text = url;
-        }
-        catch (Exception e)
-        {
-            await SkEditorAPI.Windows.ShowError("Something went wrong.\nDo you have an internet connection? Did you enter correct API key?\n\n" + e.Message);
+                JObject jsonResult = JObject.Parse(responseString);
+                string url = "https://parser.skunity.com/" + jsonResult["result"]["share_code"].ToString();
+                window.ResultTextBox.Text = url;
+            }
+            catch (Exception e)
+            {
+                await SkEditorAPI.Windows.ShowError("Something went wrong.\nDo you have an internet connection? Did you enter correct API key?\n\n" + e.Message);
+            }
         }
     }
 }
