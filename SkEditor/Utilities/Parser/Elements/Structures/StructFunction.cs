06c409175a860ab8d43e43382fc4b89559c26802
Sky <livegogo.16334@gmail.com>
2024-09-07 15:36:12 +0200

    :sparkles: Added function structure to the parser

---
+++ SkEditor/Utilities/Parser/Elements/Structures/StructFunction.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using SkEditor.API;
+
+namespace SkEditor.Parser.Elements;
+
+public partial class StructFunction : Element
+{
+    public Function InternalFunction { get; private set; }
+    
+    public override void Load(Node node, ParsingContext context)
+    {
+        var match = FunctionRegex().Match(node.Key);
+        if (!match.Success)
+        {
+            context.Warning(node, UnknownElement);
+            return;
+        }
+
+        var isLocal = match.Groups["local"].Success;
+        var name = match.Groups["name"].Value;
+        var returnType = match.Groups["returnType"].Value;
+        var parameters = new List<FunctionParameter>();
+        
+        var rawParameters = match.Groups["parameters"].Value.Split(",");
+        foreach (var rawParameter in rawParameters)
+        {
+            var parameterMatch = ParameterRegex().Match(rawParameter);
+            if (!parameterMatch.Success)
+            {
+                context.Warning(node, UnknownElement);
+                return;
+            }
+            
+            var parameterName = parameterMatch.Groups["name"].Value;
+            var parameterType = parameterMatch.Groups["type"].Value;
+            var defaultValue = parameterMatch.Groups["defaultValue"].Value;
+            parameters.Add(new FunctionParameter(parameterName, parameterType, defaultValue));
+        }
+
+        InternalFunction = new Function(name, returnType, parameters, isLocal);
+    }
+    
+    public static bool Parse(Node node)
+    {
+        return node is { IsTopLevel: true, IsSection: true } && (node.Key.StartsWith("local function") || node.Key.StartsWith("function"));
+    }
+
+    #region Data Structures
+
+    /// <summary>
+    /// Represent a function parameter.
+    /// </summary>
+    public record FunctionParameter(string Name, string Type, string? DefaultValue);
+    
+    /// <summary>
+    /// Represent a function.
+    /// </summary>
+    public record Function(string Name, string? ReturnType, List<FunctionParameter> Parameters, bool IsLocal);
+
+    #endregion
+    
+    #region Regex Patterns
+
+    // function regex
+    [GeneratedRegex(@"(?<local>local\s+)?function\s+(?<name>\w+)\((?<parameters>.*)\)(\s*(::|returns)\s*(?<returnType>\w+))?")]
+    private static partial Regex FunctionRegex();
+    
+    // parameter regex
+    [GeneratedRegex(@"(?<name>\w+):\s*(?<type>\w+)\s*(=\s*(?<defaultValue>[^)]+))?")]
+    private static partial Regex ParameterRegex();
+    
+    #endregion
+    
+    public override string Debug()
+    {
+        return $"{(InternalFunction.IsLocal ? "local " : "")}function {InternalFunction.Name}({string.Join(", ", InternalFunction.Parameters)}) :: {InternalFunction.ReturnType}";
+    }
+}
