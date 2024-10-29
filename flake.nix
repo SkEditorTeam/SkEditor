e0f4f44624ea9344d5685b2cd57dd5030763ad5b
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-29 15:04:12 +1100

    Nix init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        lib,
+        pkgs,
+        system,
+        config,
+        ...
+      }: 
+      {
+        devShells.default = pkgs.mkShell
+        {
+					packages = with pkgs; [
+						dotnet-sdk_8
+					];
+        };
+      };
+    };
+}
