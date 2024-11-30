be583a81d368b9f46425a5fbc3e8b537d14ca41d
NotroDev <notro.dev@gmail.com>
2024-11-30 16:59:54 +0100

    Correct the Program namespace

--- SkEditor/Program.cs
+++ SkEditor/Program.cs
@@ -2,45 +2,44 @@ using Avalonia;
 using Avalonia.Data;
 using System;
 
-namespace SkEditor.Desktop
+namespace SkEditor;
+
+class Program
 {
-    class Program
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            GC.KeepAlive(typeof(RelativeSource));
-            CheckTest(args);
+        GC.KeepAlive(typeof(RelativeSource));
+        CheckTest(args);
 
-            BuildAvaloniaApp()
-                    .StartWithClassicDesktopLifetime(args);
-        }
-
-        private static void CheckTest(string[] args)
-        {
-            if (args.Length > 0 && args[0] == "--test")
-            {
-                try
-                {
-                    Console.WriteLine("Test passed");
-                }
-                catch (Exception e)
-                {
-                    Console.Error.WriteLine($"Test failed: {e.Message}");
-                    Environment.Exit(1);
-                }
-                return;
-            }
-        }
-
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToTrace()
-                .WithInterFont();
+        BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
     }
+
+    private static void CheckTest(string[] args)
+    {
+        if (args.Length > 0 && args[0] == "--test")
+        {
+            try
+            {
+                Console.WriteLine("Test passed");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Test failed: {e.Message}");
+                Environment.Exit(1);
+            }
+            return;
+        }
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .WithInterFont();
 }
