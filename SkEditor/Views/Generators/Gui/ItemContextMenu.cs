55e6ba629af5daa8a617aa1049d30762fde4dae1
NotroDev <notro.dev@gmail.com>
2025-05-07 11:55:29 +0200

    fix #159

--- SkEditor/Views/Generators/Gui/ItemContextMenu.cs
+++ SkEditor/Views/Generators/Gui/ItemContextMenu.cs
@@ -48,6 +48,7 @@ internal class ItemContextMenu
         EditedItem = slot == -1
             ? generator.BackgroundItem
             : generator.Items.GetValueOrDefault(slot);
+        
         Item? item = await generator.SelectItem();
         if (item == null)
         {
