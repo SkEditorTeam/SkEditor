9d1b96062f0c8e74887637c1a128ec3a8612d4d0
NotroDev <notro.dev@gmail.com>
2025-07-05 18:13:19 +0200

    Add error handling to addon update checking

--- SkEditor/Utilities/AddonUpdateChecker.cs
+++ SkEditor/Utilities/AddonUpdateChecker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAvalonia.UI.Controls;
 using SkEditor.API;
@@ -11,23 +12,30 @@ public static class AddonUpdateChecker
 {
     public static async Task CheckForUpdates()
     {
-        IAsyncEnumerable<MarketplaceItem> items = MarketplaceLoader.GetItems();
-
-        await foreach (MarketplaceItem item in items)
+        try
         {
-            if (item is not AddonItem addonItem) continue;
+            IAsyncEnumerable<MarketplaceItem> items = MarketplaceLoader.GetItems();
 
-            var currentVersion = addonItem.GetAddon()?.Version;
-            var latestVersion = item.ItemVersion;
-
-            if (currentVersion == null || latestVersion == null) continue;
-
-            if (currentVersion != latestVersion)
+            await foreach (MarketplaceItem item in items)
             {
-                await SkEditorAPI.Windows.ShowDialog(Translation.Get("AddonUpdateAvailableTitle"),
-                    Translation.Get("AddonUpdateAvailableMessage", addonItem.ItemName),
-                    Symbol.AlertUrgent);
+                if (item is not AddonItem addonItem) continue;
+
+                var currentVersion = addonItem.GetAddon()?.Version;
+                var latestVersion = item.ItemVersion;
+
+                if (currentVersion == null || latestVersion == null) continue;
+
+                if (currentVersion != latestVersion)
+                {
+                    await SkEditorAPI.Windows.ShowDialog(Translation.Get("AddonUpdateAvailableTitle"),
+                        Translation.Get("AddonUpdateAvailableMessage", addonItem.ItemName),
+                        Symbol.AlertUrgent);
+                }
             }
+        }
+        catch
+        {
+            // ignore - we won't bother the user, if he has no internet connection or something else goes wrong
         }
     }
 }
